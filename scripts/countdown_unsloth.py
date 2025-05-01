0354b4149befe55558374334c7f4fd6c1b117125
Kristjan Kongas <kongaskristjan@gmail.com>
2025-05-02 01:21:41 +0300

    Add num_iterations=3

--- scripts/countdown_unsloth.py
+++ scripts/countdown_unsloth.py
@@ -64,6 +64,7 @@ def main() -> None:
         max_prompt_length=256,
         max_completion_length=1024,  # max length of the generated output for our solution
         num_generations=4,
+        num_iterations=3,
         beta=0.05,
         #        scale_rewards=False,
         #        loss_type="dr_grpo",
